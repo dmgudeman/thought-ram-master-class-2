3af443ff0a9a8fbadea414f002df6f8eba82aef0
Jeff Anderson <andersjh1120@gmail.com>
2017-08-24 08:20:01 -0500

    added tabs exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,10 +12,19 @@ import {HttpClientModule} from '@angular/common/http';
 import { ContactsEditorComponent } from './contacts-editor/contacts-editor.component';
 import {FormsModule} from '@angular/forms';
 import { ContactsDetailViewComponent } from './contacts-detail-view/contacts-detail-view.component';
+import { TabComponent} from './tabs/tab/tab.component';
+import { TabsComponent} from './tabs/tabs/tabs.component';
 
 
 @NgModule({
-  declarations: [ContactsAppComponent, ContactsListComponent, ContactsDetailComponent, ContactsEditorComponent, ContactsDetailViewComponent],
+  declarations: [
+    ContactsAppComponent,
+    ContactsListComponent,
+    ContactsDetailComponent,
+    ContactsEditorComponent,
+    ContactsDetailViewComponent,
+    TabComponent,
+    TabsComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
